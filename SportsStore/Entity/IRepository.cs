87ce382a927726f230bec8feab689273f48d8ff5
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-11-20 20:45:10 +0200

    add repository interface

---
+++ SportsStore/Entity/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using SportsStore.Models;
+using System.Collections.Generic;
+
+namespace SportsStore.Entity
+{
+    public interface IRepository
+    {
+        IEnumerable<Product> Products { get; }
+        void AddProduct(Product product);
+    }
+}
