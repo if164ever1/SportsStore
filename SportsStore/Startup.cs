a741d1e611341d5a20f3ffdb3ec497d02e54b276
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-11-20 20:57:56 +0200

    add dependency

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -1,13 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using SportsStore.Entity;
+
 
 namespace SportsStore
 {
@@ -25,6 +22,7 @@ namespace SportsStore
         {
             services.AddRazorPages();
             services.AddMvc();
+            services.AddSingleton<IRepository, DataRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
