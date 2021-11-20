1c03dd508c8a9e07fbb2b7951a0c6e748c8c1335
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-11-20 21:02:33 +0200

    add homecontroller

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Entity;
+using SportsStore.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,21 @@ namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
+        private IRepository repository;
+        public HomeController(IRepository repo)
+        {
+            repository = repo;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(repository.Products);
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            repository.AddProduct(product);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
